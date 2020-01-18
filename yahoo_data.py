2816c3d8f3db7759eb24793b1cf57ab62e9cb9e0
Ryan (Chiungting) Lin <ryanctlin@hotmail.com>
2020-01-18 17:24:16 +0000

    Cleaned Yahoo scraper

--- yahoo_data.py
+++ yahoo_data.py
@@ -2,18 +2,30 @@ import requests
 from bs4 import BeautifulSoup
 import re
 
-def scrape_contents(url):
-    """This function checks and accepts a Yahoo finance URL, """
+def ticker_ESG(ticker):
+    """This function accepts a ticker input and outputs its Total ESG Risk Score"""
+    #Generate URL
+    working_url = 'https://uk.finance.yahoo.com/quote/'+str(ticker).upper()+'/sustainability?p='+str(ticker).upper()
+    total_ESG = scrape_yahoo(working_url)
+    return total_ESG
+
+def scrape_yahoo(url):
+    """This function checks and accepts a Yahoo finance URL and outputs the Total ESG Risk Score as an int """
 
     #GET page and convert content to bs4 soup
     page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
 
-    total_ESG = soup.findAll('div', {"class": "Fz(36px) Fw(600) D(ib) Mend(5px)"})
-    #total_ESG = total_ESG.get_text()  # Extract text from bs4 tag object
-    return str(total_ESG)
 
-result = scrape_contents('https://uk.finance.yahoo.com/quote/AM/sustainability?p=AM')
+    #Find class containing ESG risk score from 0-100, cast to string
+    total_ESG = str(soup.findAll('div', {"class": "Fz(36px) Fw(600) D(ib) Mend(5px)"}))
 
-result = re.search('data-reactid="20">(.+?)</div>', result).group(1)
-print(result)
+    #Extract ESG score using regex, with error handling
+    if total_ESG != '[]':
+        total_ESG = int(re.search('data-reactid="20">(.+?)</div>', total_ESG).group(1))
+    else:
+        total_ESG = None
+
+    return total_ESG
+
+print(ticker_ESG('aapl'))
